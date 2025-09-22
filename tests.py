1a5b7fcacca85ef429add99faf2cbe2c54fdb2f3
Daryl <bragouel@gmail.com>
2025-09-22 13:44:00 +0200

    - Updated the calculator tests - created the function to empower the llm to run python file (for the moment)

--- tests.py
+++ tests.py
@@ -1,9 +1,13 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def main():
     working_directory = "calculator"
+    run_test =  run_python_file("calculator", "main.py", ["4 * 5"])
+    print(run_test)
+
     # write_test = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     # print(write_test)
 
