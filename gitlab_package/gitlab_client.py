9d7d3dd30250f7acf0e648a0daa0f9377ee18799
Daryl <bragouel@gmail.com>
2025-10-01 10:03:52 +0200

    add create_commit method to GitlabClient for creating commits and update source_branch in merge request

--- gitlab_package/gitlab_client.py
+++ gitlab_package/gitlab_client.py
@@ -1,7 +1,8 @@
 import gitlab
 import logging
 import time
-from gitlab_package.config import GITLAB_PROJECT_ID, GITLAB_PRIVATE_TOKEN, GITLAB_URL
+# from gitlab_package.config import GITLAB_PROJECT_ID, GITLAB_PRIVATE_TOKEN, GITLAB_URL
+from config import GITLAB_PROJECT_ID, GITLAB_PRIVATE_TOKEN, GITLAB_URL
 
 class GitlabClient:
     def __init__(self):
@@ -18,13 +19,28 @@ class GitlabClient:
         # mr_description_template = project.merge_request_templates.get("default")
         merge_request = self.project.mergerequests.create(
             {
-                'source_branch': '',
+                'source_branch': 'ai_branch',
                 'target_branch': 'main',
                 'title': "issue_fix",
                 'labels': ['ai:agent'],
                 # 'description': mr_description_template.content
             }
         )
+
+    def create_commit(self):
+        data = {
+            'branch': 'ai_branch',
+            'commit_message': 'created a hello world script', #TODO AI message
+            'actions': [
+                {
+                    'action': 'create',
+                    'file_path': 'hello.py',
+                    'content': 'print("hello, world")\n'
+                }
+            ]
+        }
+        self.project.commits.create(data)
+
 
 def look_for_issues(client):
     #TODO take the time in consideration
@@ -34,8 +50,11 @@ def look_for_issues(client):
         issue_details =f"title: {issue.title}, description: {issue.description}".strip()
         return issue_details
 
-# if __name__ == "__main__":
-#     client = GitlabClient()
+if __name__ == "__main__":
+    client = GitlabClient()
+    # client.create_commit()
+    print(client.project.commits.list(ref_name='ai_branch',get_all=True))
+    # client.create_merge_request()
 #     while True:
 #         look_for_issues(client)
 
